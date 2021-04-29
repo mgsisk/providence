e85fbcf490733aee59ec4345b65f6b7daebae3b1
Michael Sisk <me@mgsisk.com>
2021-04-29 10:10:19 -0400

    Create mailhog.sh

---
+++ src/bin/mailhog.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Install MailHog.
+# ------------------------------------------------------------------------------
+
+if grep -qw mailhog /tmp/prov
+  then echo 'Installing MailHog'
+  wget -nc -qO /tmp/mailhog.v https://api.github.com/repos/mailhog/mailhog/releases/latest
+  wget -nc -qO /usr/local/bin/mailhog "$(grep browser_download_url /tmp/mailhog.v | grep "$(uname -s | tr '[:upper:]' '[:lower:]')_$(uname -r | rev | cut -d- -f1 | rev)" | cut -d\" -f4)"
+  chmod +x /usr/local/bin/mailhog
+fi
