db2300cb155af006e9fcd87f8918321e84ad3407
Michael Sisk <me@mgsisk.com>
2021-04-29 10:10:40 -0400

    Create rustup.sh

---
+++ src/bin/rustup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# shellcheck disable=1090 disable=1091 disable=2034
+#
+# Install rustup and related packages.
+# ------------------------------------------------------------------------------
+
+if grep -qw rust /tmp/prov
+  then echo 'Installing rustup'
+  export CARGO_HOME=$VUD/.cargo RUSTUP_HOME=$VUD/.rustup
+  wget -nc -qO /usr/local/bin/rustup-init "https://static.rust-lang.org/rustup/dist/$(uname -m)-unknown-linux-gnu/rustup-init"
+  chmod +x /usr/local/bin/rustup-init
+  rustup-init -y >/dev/null 2>/dev/null
+
+  echo "$PATH" | grep -q '/\.cargo/' || . "$VUD/.cargo/env"
+  grep -q '\.cargo' .bash_profile || echo '. ~/.cargo/env' >>.bash_profile
+
+  if [ -n "$RUST_VER" ] && ! rustup default | cut -d- -f1 | grep -qw "$RUST_VER"
+    then echo "Installing Rust $RUST_VER"
+    rustup toolchain install "$RUST_VER" >/dev/null 2>/dev/null
+    rustup default "$RUST_VER" >/dev/null 2>/dev/null
+  fi
+fi
