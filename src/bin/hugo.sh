ccf0e7394da0abc25508403e4a8425f2abcd7df5
Michael Sisk <me@mgsisk.com>
2021-04-29 10:10:17 -0400

    Create hugo.sh

---
+++ src/bin/hugo.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Install Hugo and related packages.
+# ------------------------------------------------------------------------------
+
+if grep -qw hugo /tmp/prov
+  then echo 'Installing Hugo'
+  mkdir -p /usr/local/lib/hugo
+  wget -nc -qO /tmp/hugo.v https://api.github.com/repos/gohugoio/hugo/releases/latest
+  wget -nc -qO /tmp/hugo.tar.gz "$(grep browser_download_url /tmp/hugo.v | grep -m1 "Linux-$(file /bin/dpkg | grep -o '[0-9]*-bit' | tr -d '-')\.tar.gz" | cut -d\" -f4)"
+  tar -xkf /tmp/hugo.tar.gz -C /usr/local/lib/hugo 2>/dev/null
+  ln -fs /usr/local/lib/hugo/hugo /usr/local/bin/huo
+fi
