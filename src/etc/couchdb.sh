9a5311f5d5f395ec05068a946670dc2e5009429e
Michael Sisk <me@mgsisk.com>
2021-04-29 10:10:59 -0400

    Create couchdb.sh

---
+++ src/etc/couchdb.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Configure CouchDB.
+# ------------------------------------------------------------------------------
+
+if dpkg --get-selections | grep -q '^couchdb.*\si'
+  then echo 'Configuring CouchDB'
+  cd /opt/couchdb || exit
+  mkdir -p /srv/sys/couchdb
+  echo "<meta http-equiv='refresh' content='0; url=http://$(hostname -f):5984/_utils/'>" >/srv/sys/couchdb/index.html
+  sed -i 's/^;admin = .*/root = vagrant/' etc/local.ini
+  sed -i 's/^;bind_address = .*/bind_address = 0.0.0.0/' etc/local.ini
+  sed -i 's/^;WWW-Authenticate =/WWW-Authenticate =/' etc/local.ini
+  service couchdb restart
+  cd "$VUD" || exit
+fi
