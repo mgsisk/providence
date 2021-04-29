7d53aa0395dff721b04d5cfc7b62902319fc0f32
Michael Sisk <me@mgsisk.com>
2021-04-29 15:03:36 -0400

    Update make.sh

--- src/make.sh
+++ src/make.sh
@@ -11,6 +11,6 @@ src/bin/*.sh \
 src/etc/*.sh \
 src/opt/*.sh \
 src/term.sh  \
->dist/provisioner.sh
-sed -i '' '/^==>.*/d' dist/provisioner.sh
-sed -i '' '1s|^|#!/bin/sh\n|' dist/provisioner.sh
+>dist/merge.sh
+sed '/^==>.*/d' dist/merge.sh | sed '1s|^|#!/bin/sh\n|' >dist/provisioner.sh
+rm -f dist/merge.sh
