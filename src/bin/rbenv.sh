020d3d94553ed297a7d909c4f675a1487b026bd4
Michael Sisk <me@mgsisk.com>
2021-04-29 10:10:37 -0400

    Create rbenv.sh

---
+++ src/bin/rbenv.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# shellcheck disable=1090 disable=1091
+#
+# Install rbenv and related packages.
+# ------------------------------------------------------------------------------
+
+if grep -qw ruby /tmp/prov
+ then echo 'Installing ruby-build'
+  mkdir -p .ruby-build .rbenv
+  wget -nc -qO /tmp/ruby-build.tar.gz https://api.github.com/repos/rbenv/ruby-build/tarball
+  tar -xkf /tmp/ruby-build.tar.gz -C .ruby-build --strip-components 1 2>/dev/null
+  .ruby-build/install.sh
+
+  echo 'Installing rbenv'
+  wget -nc -qO /tmp/rbenv.tar.gz https://api.github.com/repos/rbenv/rbenv/tarball
+  tar -xkf /tmp/rbenv.tar.gz -C .rbenv --strip-components 1 2>/dev/null
+  cd .rbenv || exit
+  src/configure
+  make -sC src
+  cd .. || exit
+
+  cat <<_ >.bash_rbenv
+export RBENV_ROOT=$VUD/.rbenv
+export PATH=\$RBENV_ROOT/bin:\$PATH
+eval "\$(rbenv init -)"
+_
+
+  [ -n "$RBENV_ROOT" ] || . "$VUD/.bash_rbenv"
+  grep -q '\.bash_rbenv' .bash_profile || echo '. ~/.bash_rbenv' >>.bash_profile
+  grep -q '\.bash_rbenv' /root/.bash_profile || echo ". $VUD/.bash_rbenv" >>/root/.bash_profile
+
+  if [ -n "$RUBY_VER" ] && ! rbenv global | grep -qw "$RUBY_VER"
+    then echo "Installing Ruby $RUBY_VER"
+    rbenv install "$RUBY_VER" 2>/dev/null
+    rbenv global "$RUBY_VER"
+  fi
+
+  echo 'Installing Bundler'
+  rbenv exec gem install -N bundler >/dev/null
+
+  if [ -s "$BUNDLER_CNF" ]
+    then echo 'Installing Bundler packages'
+    cd "$(dirname "$BUNDLER_CNF")" || exit
+    rbenv exec bundle install --quiet
+    cd "$VUD" || exit
+  fi
+fi
