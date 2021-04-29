921b013809ec78dda29dddf9c074891208d5756f
Michael Sisk <me@mgsisk.com>
2021-04-29 10:11:27 -0400

    Create docker.sh

---
+++ src/opt/docker.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Build Docker images.
+# ------------------------------------------------------------------------------
+
+if grep -qw docker /tmp/prov
+  then if [ -s "$DOCKER_COMPOSE_CNF" ]
+    then cd "$(dirname "$DOCKER_COMPOSE_CNF")" || exit
+    echo 'Creating Docker environment'
+    docker-compose up -d 2>/dev/null
+    cd "$VUD" || exit
+  elif [ -e "$DOCKER_CNF" ]
+    then cd "$(dirname "$DOCKER_CNF")" || exit
+    echo 'Creating Docker image'
+    docker run -d "$(docker build -q .)" >/dev/null
+    cd "$VUD" || exit
+  fi
+fi
