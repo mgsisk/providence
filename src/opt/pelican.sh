f665e9644d080cd9b5bf6d859f60d4ae3261f3b7
Michael Sisk <me@mgsisk.com>
2021-05-10 11:04:28 -0400

    Update pelican.sh

--- src/opt/pelican.sh
+++ src/opt/pelican.sh
@@ -7,8 +7,8 @@ if [ -d "$PELICAN_DIR" ] && grep -qw pelican /tmp/prov
   then cd "$PELICAN_DIR" || exit
   if pyenv which pipenv >/dev/null 2>&1 && pyenv exec pipenv graph 2>/dev/null | grep -q ^pelican=
     then echo 'Creating Pelican site'
-    [ -z "$PELICAN_CNF" ] && PELICAN_CNF=$(find "$PELICAN_DIR" -maxdepth 1 -name '*conf.py' | head -n1)
-    [ -z "$PELICAN_SRV" ] && PELICAN_SRV=/srv/web$(pyenv exec pelican -s "$PELICAN_CNF" --print-settings SITEURL | tr -d "'\n" | cut -d/ -f4- | printf '/%s' "$(cat -)" | sed 's|/$||')
+    [ -n "$PELICAN_CNF" ] || PELICAN_CNF=$(find "$PELICAN_DIR" -maxdepth 1 -name '*conf.py' | head -n1)
+    [ -n "$PELICAN_SRV" ] || PELICAN_SRV=/srv/web$(pyenv exec pelican -s "$PELICAN_CNF" --print-settings SITEURL | tr -d "'\n" | cut -d/ -f4- | printf '/%s' "$(cat -)" | sed 's|/$||')
     rm -rf "$PELICAN_SRV"
     pyenv exec pipenv run pelican -q -s "$PELICAN_CNF" -e OUTPUT_DIR="$PELICAN_SRV" SITEURL="http://$(hostname -f)${PELICAN_SRV#/srv/web}" >/dev/null
 
