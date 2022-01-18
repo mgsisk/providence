25f887237ec04f4d11f196bcba8a4e39c273b9e4
Michael Sisk <me@mgsisk.com>
2022-01-18 10:19:07 -0500

    Fix quotes (#17)
    * Update CHANGELOG.md
    
    * Update postgresql.sh
    
    * Update term.sh

--- src/etc/postgresql.sh
+++ src/etc/postgresql.sh
@@ -6,8 +6,8 @@
 if dpkg --get-selections | grep -q '^postgresql.*\si'
   then echo 'Configuring PostgreSQL'
   vpsql=$(psql --version | cut -d' ' -f3 | cut -d. -f1)
-  sed -i "s/#listen_addresses = .*/listen_addresses = '*'/" /etc/postgresql/"$vpsql"/main/postgresql.conf
-  sed -i 's/ peer$/ trust/g' /etc/postgresql/"$vpsql"/main/pg_hba.conf && service postgresql restart
+  sed -i "s/#listen_addresses = .*/listen_addresses = '*'/" "/etc/postgresql/$vpsql/main/postgresql.conf"
+  sed -i 's/ peer$/ trust/g' "/etc/postgresql/$vpsql/main/pg_hba.conf" && service postgresql restart
   psql -U postgres -c '\du' | grep -q root || psql -U postgres -c "create role root with superuser password 'vagrant' login" >/dev/null
   service postgresql restart
 fi
